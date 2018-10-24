152505b44c2ba8cf1c21387a905f65f7200a8cce
buhtib <2418531916@qq.com>
2018-10-25 01:08:04 +0800

    基本完成首部

--- js/index.js
+++ js/index.js
@@ -12,13 +12,8 @@ var meituan = (function () {
         response() {
             var _this = this;
             var devicewidth = $('html').width();
+            $('html').css('font-size' , devicewidth / 3.75 + 'px');
             
-            if (devicewidth >= 375) {
-                $('html').css('font-size' , 100 + 'px')
-            }else {
-                $('html').css('font-size' , devicewidth / 3.75 + 'px');
-            }
-            console.log($('html').css('font-size'));
             window.addEventListener('load', _this.response);
             window.addEventListener('resize',_this.response);
 
