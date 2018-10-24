4eeedf78340b65b5a48ab11ab73c792920b459c6
buhtib <2418531916@qq.com>
2018-10-24 09:02:54 +0800

    初始化js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let changefont = ()=>{
-    let devicewidth = document.documentElement.clientWidth;
-    console.log(devicewidth);
-    document.documentElement.style.fontSize = devicewidth / 3.5 + 'px';
-}
-window.addEventListener('load', changefont);
-window.addEventListener('resize', changefont);
+var meituan = (function() {
+    return {
+        init() {
+            this.response();
+            this.event();
+
+        },
+        event() {
+            var _this = this;
+        },
+        response() {
+            var  changefont = function() {
+                let devicewidth = document.documentElement.clientWidth;
+                document.documentElement.style.fontSize = devicewidth / 3.5 + 'px';
+                console.log(devicewidth, document.documentElement.style.fontSize);
+            }
+            window.addEventListener('load', changefont);
+            window.addEventListener('resize', changefont);
+            
+        }
+    }
+}())
