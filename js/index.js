52455bb4b3f1fcb73f0bc632347d6eb558306ba2
zreal-leo <2504846414@qq.com>
2018-10-25 20:04:29 +0800

    v1.0

--- js/index.js
+++ js/index.js
@@ -9,11 +9,11 @@ var meituan = (function () {
             var _this = this;
         },
 
+        // 实现html的font-size动态更新
         response() {
             var _this = this;
             var devicewidth = $('html').width();
             $('html').css('font-size' , devicewidth / 3.75 + 'px');
-            
             window.addEventListener('load', _this.response);
             window.addEventListener('resize',_this.response);
 
